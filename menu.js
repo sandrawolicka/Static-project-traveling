8b6a9cac7c37f3fa7357cd302f42840fc0b288f1
sandrawolicka <sandra.wolicka1234@gmail.com>
2022-11-30 15:07:04 +0100

    commit

---
+++ menu.js
@@ -0,0 +1,21 @@
+const mobileNav = document.querySelector('ul');
+const burgerIcon = document.querySelector('.burger');
+
+let miniMenuShowed = false;
+
+burgerIcon.addEventListener('click', function(){
+  miniMenuShowed = !miniMenuShowed
+  mobileNav.classList.toggle('active');
+  burgerIcon.classList.toggle('active');
+  mobileNav.classList.toggle('displayblock');
+})
+
+
+onresize = (event) => {
+  if(window.innerWidth > 1000 && miniMenuShowed) {
+    mobileNav.classList.remove('active');
+    burgerIcon.classList.remove('active');
+    mobileNav.classList.remove('displayblock');
+    miniMenuShowed = false;
+  }
+};
